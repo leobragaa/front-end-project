8cae03c9e1e4cfa71712ef52003655cc25dd8e4f
leobragaa <leombraga11@gmail.com>
2026-09-21 14:49:37 -0300

    ♻️ Refazendo a tela de login e deixando responsiva

--- README.md
+++ README.md
@@ -20,6 +20,18 @@ You can start editing the page by modifying `app/page.tsx`. The page auto-update
 
 This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
 
+## Bibliotecas
+
+```bash
+
+```
+
+| Biblioteca | Links                  |
+| ---------: | ---------------------- |
+|     Chakra | https://chakra-ui.com/ |
+|      Axios | https://axios.rest/    |
+|     Formik | https://formik.org/    |
+
 ## Learn More
 
 To learn more about Next.js, take a look at the following resources:
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 "use client";
 
 import { realizarLogin } from "@/actions/login";
+
 import {
+  Box,
   Button,
   Card,
   Center,
   CheckboxCard,
   Field,
+  Flex,
   Grid,
   GridItem,
   Image,
   Input,
   Stack,
+  Strong,
+  Text,
 } from "@chakra-ui/react";
 import { Formik } from "formik";
 import { useRouter } from "next/navigation";
@@ -19,115 +24,138 @@ import { useRouter } from "next/navigation";
 export default function HomePage() {
   const router = useRouter();
   return (
-    <Center>
-      <Formik
-        initialValues={{ email: "", senha: "" }}
-        onSubmit={async (values, { setSubmitting }) => {
-          console.log(values);
-          try {
-            const usuario = await realizarLogin(values.email, values.senha);
-            console.log("Dentro do form", usuario);
-            router.push("/dashboard");
-          } catch (error) {
-            console.log("Erro em logar");
-          }
-          setSubmitting(false);
-        }}
-      >
-        {({
-          values,
-          errors,
-          touched,
-          handleChange,
-          handleBlur,
-          handleSubmit,
-          isSubmitting,
-          /* and other goodies */
-        }) => (
-          <form onSubmit={handleSubmit}>
-            <Grid templateColumns="repeat(2, 1fr)" gap="8">
-              <GridItem colSpan={1}>
-                <Image
-                  width="full"
-                  src="NutriFlow.png"
-                  alt="Logo lateral NutriFlow"
-                />
-              </GridItem>
-
-              <GridItem colSpan={1}>
-                <Center alignItems="center" marginTop="100px">
-                  <Card.Root width="500px">
-                    <Card.Header alignItems="center">
-                      <Image
-                        justifyContent="center"
-                        src="NutriFlowSFun.png"
-                        boxSize="50px"
-                        borderRadius="10px"
-                        fit="cover"
-                        alt="Logo do topo NutriFlow"
-                      />
-                      <Card.Title>Acesse sua Conta </Card.Title>
-                      <Card.Description margin="4px">
-                        no NutriFlow
-                      </Card.Description>
-                    </Card.Header>
-                    <Card.Body>
-                      <Stack gap="4">
-                        <Field.Root>
-                          <Field.Label color="green.600" fontSize="15px">
-                            <strong>E-MAIL :</strong>
-                          </Field.Label>
-                          <Input
-                            placeholder="seu-email@example.com"
-                            type="email"
-                            name="email"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.email}
-                          />
-                          {errors.email && touched.email && errors.email}
-                        </Field.Root>
-
-                        <Field.Root>
-                          <Field.Label color="green.600" fontSize="15px">
-                            <strong>SENHA :</strong>
-                          </Field.Label>
-                          <Input
-                            type="password"
-                            name="senha"
-                            onChange={handleChange}
-                            onBlur={handleBlur}
-                            value={values.senha}
-                          />
-                          {errors.senha && touched.senha && errors.senha}
-                        </Field.Root>
-                      </Stack>
-                      <CheckboxCard.Root maxW="240px" marginTop="10px">
-                        <CheckboxCard.HiddenInput />
-                        <CheckboxCard.Control>
-                          <CheckboxCard.Indicator />
-                          <CheckboxCard.Label>
-                            Aceito os Termos
-                          </CheckboxCard.Label>
-                        </CheckboxCard.Control>
-                      </CheckboxCard.Root>
-                    </Card.Body>
-                    <Card.Footer justifyContent="flex-end">
-                      <Button
-                        type="submit"
-                        variant="solid"
-                        disabled={isSubmitting}
-                      >
-                        Acessar
-                      </Button>
-                    </Card.Footer>
-                  </Card.Root>
+    <Formik
+      initialValues={{ email: "", senha: "" }}
+      onSubmit={async (values, { setSubmitting }) => {
+        console.log(values);
+        try {
+          const usuario = await realizarLogin(values.email, values.senha);
+          console.log("Dentro do form", usuario);
+          router.push("/dashboard");
+        } catch (error) {
+          console.log("Erro em logar");
+        }
+        setSubmitting(false);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+        /* and other goodies */
+      }) => (
+        <form onSubmit={handleSubmit}>
+          {/* <Center> */}
+          <Grid
+            flexDirection={["column", "column", "row", "row"]}
+            templateColumns="repeat(5, 1fr)"
+            height={"100vh"}
+            gap="3"
+          >
+            <GridItem
+              colSpan={[5, 5, 3, 3]}
+              background="green.900"
+              flexDirection="column"
+            >
+              <Flex direction={["column"]} alignItems={"center"}>
+                <Box marginTop={"20px"}>
+                  <Image
+                    maxW="sm"
+                    src="NutriFlowSFun.png"
+                    alt="Logo lateral NutriFlow"
+                  />
+                </Box>
+                <Flex marginTop={"50px"}>
+                  <Text fontFamily="sans-serif" fontSize="50px">
+                    Nutri
+                  </Text>
+                  <Text fontFamily="sans-serif" fontSize="50px">
+                    Flow
+                  </Text>
+                </Flex>
+                <Center gap="1">
+                  <Box>NUTRIÇÃO °</Box>
+                  <Box>PERFORMANCE °</Box>
+                  <Box>EQUILIBRIO </Box>
                 </Center>
-              </GridItem>
-            </Grid>
-          </form>
-        )}
-      </Formik>
-    </Center>
+              </Flex>
+            </GridItem>
+
+            <GridItem colSpan={[5, 5, 2, 2]} marginLeft={"5"} marginRight={"5"}>
+              <Card.Root marginTop={["0px", "0px", "24", "24"]}>
+                <Card.Header alignItems="center">
+                  <Image
+                    justifyContent="center"
+                    src="NutriFlowSFun.png"
+                    boxSize="50px"
+                    borderRadius="10px"
+                    alt="Logo do topo NutriFlow"
+                  />
+                  <Card.Title>Acesse sua Conta </Card.Title>
+                  <Card.Description margin="2px">no NutriFlow</Card.Description>
+                </Card.Header>
+                <Card.Body>
+                  <Stack gap="4">
+                    <Field.Root>
+                      <Field.Label color="green.600" fontSize="15px">
+                        <strong>E-MAIL :</strong>
+                      </Field.Label>
+                      <Input
+                        placeholder="seu-email@example.com"
+                        type="email"
+                        name="email"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                      />
+                      {errors.email && touched.email && errors.email}
+                    </Field.Root>
+
+                    <Field.Root>
+                      <Field.Label color="green.600" fontSize="15px">
+                        <strong>SENHA :</strong>
+                      </Field.Label>
+                      <Input
+                        type="password"
+                        name="senha"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.senha}
+                      />
+                      {errors.senha && touched.senha && errors.senha}
+                    </Field.Root>
+                  </Stack>
+                  <CheckboxCard.Root maxW="240px" marginTop="10px">
+                    <CheckboxCard.HiddenInput />
+                    <CheckboxCard.Control>
+                      <CheckboxCard.Indicator />
+                      <CheckboxCard.Label>Aceito os Termos</CheckboxCard.Label>
+                    </CheckboxCard.Control>
+                  </CheckboxCard.Root>
+                </Card.Body>
+                <Card.Footer justifyContent="center">
+                  <Button
+                    background="orange.600"
+                    color="white"
+                    width="50%"
+                    cursor="pointer"
+                    type="submit"
+                    variant="solid"
+                    disabled={isSubmitting}
+                  >
+                    <Strong> ENTRAR </Strong>
+                  </Button>
+                </Card.Footer>
+              </Card.Root>
+            </GridItem>
+          </Grid>
+          {/* </Center> */}
+        </form>
+      )}
+    </Formik>
   );
 }
